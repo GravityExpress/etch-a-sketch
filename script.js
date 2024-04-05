535b4afa2abd37e18c96db2a16db4911101a2ba9
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-04-06 01:00:20 +0800

    Added drawing functionality, change grid size, and clear grid
    Created event listeners for grid cells to allow drawing. Added buttons to change grid size and to clear the canvas

--- script.js
+++ script.js
@@ -1,23 +1,74 @@
-function createGrid () 
+function createGrid(size = 16) 
 {
-    for (let i = 0; i < 16 ** 2; i++)
+    for (let i = 0; i < size ** 2; i++)
     {
         let cell = document.createElement("div");
         cell.classList.add("cells");
         grid.appendChild(cell);
     }
+    let cellSize = gridSize/size + "vh";
+    cells = document.querySelectorAll(".cells");
+    changeGridSize(cellSize, cells);
+    addGridEvent();
+}
+
+function changeGridSize(cellSize, cells)
+{
+    cells.forEach((cell) => {
+        cell.style.width = cellSize;
+        cell.style.height = cellSize;
+    });
+}
+
+function clearGridSize()
+{
+    cells.forEach((cell) => {
+        grid.removeChild(cell);
+    });
+}
+
+function addGridEvent()
+{
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover",
+            (event) => {
+                event.target.style.backgroundColor = "black";
+            }
+        );
+    });    
 }
 
 let gridSize = 50;
 let grid = document.querySelector("#grid");
 grid.style.width = gridSize + "vh";
+let cells;
 
 createGrid();
 
-let cellSize = gridSize/16 + "vh";
-let cells = document.querySelectorAll(".cells");
+let gridSizeButton = document.querySelector("#grid-size-button");
+let cellCount = 0;
 
-cells.forEach((cell) => {
-    cell.style.width = cellSize;
-    cell.style.height = cellSize;
-});
+gridSizeButton.addEventListener("click",
+    () => {
+        cellCount = Number(prompt("Enter grid size 1 - 100"))
+        if (cellCount && cellCount <= 100)
+        {
+            clearGridSize();
+            createGrid(cellCount);
+        }
+        else
+        {
+            alert("Cancelled or Invalid Amount");
+        }
+    }
+);
+
+let gridResetButton = document.querySelector("#grid-clear-button");
+
+gridResetButton.addEventListener("click",
+    () => {
+        cells.forEach((cell) => {
+            cell.style.backgroundColor = "white";
+        });
+    }
+);
